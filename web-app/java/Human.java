f4549db632643d4520581d4a4822e8fe59a2ec02
dzxdzx1987 <dzxdzx1987@nate.com>
2013-12-30 16:26:59 +0900

    web-app file

---
+++ web-app/java/Human.java
@@ -0,0 +1,15 @@
+package com.motrex.kevin.test;
+public class Human
+{
+	private String name;
+	private int age;
+	private long id;
+
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
+
+}
